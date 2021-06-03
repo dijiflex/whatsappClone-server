537d176a7d7c6b0d90c1e368d688989b5d2ea731
dijiflex <dijiflex@gmail.com>
2021-06-03 11:01:23 +0300

    setup server configuration

---
+++ server.js
@@ -0,0 +1,24 @@
+// Importing 
+import express from "express";
+
+//App configuration
+const app = express();
+
+
+// Middlewares
+
+
+/// Database Config
+
+//???? 
+
+
+//Api routes
+app.get('/', (req, res) => res.status(200).send('Hello world bro'))
+
+
+//Listners
+
+
+
+
